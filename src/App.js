b84beb2ffcce0cd04d60fbd90bf078b56a181598
baraaj <jeridibaraa@gmail.com>
2022-07-06 12:43:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import Navbar from "./components/navbar";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './navbar.css';
+import {BsFillPersonFill} from 'react-icons/bs';
+import ReactDOM from 'react-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link" href="#">Clubs</a>
+      <a className="nav-item nav-link" href="#">Salles</a>
+      <a className="nav-item nav-link" href="#">Evènements</a>
+      <a className="nav-item nav-link" href="#">Contacts</a>
+      <a className="nav-icon nav-item nav-link" href="#"><BsFillPersonFill/></a>
+    </div>
+  </div>
+</nav>
+        </div>
+    );
+}
+
+export default Navbar;
