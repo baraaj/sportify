82a176563f28c237dc3488f3ddcbb88d2062dbe1
baraaj <jeridibaraa@gmail.com>
2022-07-07 11:37:29 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './footer.css';
+import {FaRegCopyright} from 'react-icons/fa';
+const Footer = () => {
+    return (
+        
+            <div className="foot">
+            <div className='footer'> 
+            <div className=''>
+                <></>
+            </div> 
+            <div className='copyright'>
+            <span><FaRegCopyright/> Copyrights 2022</span>
+            </div>
+            </div>
+            </div>
+        
+    );
+}
+
+export default Footer;
