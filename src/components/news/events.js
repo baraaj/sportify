02e12947bf3c0ff9122b0699077282d376e394fd
baraaj <jeridibaraa@gmail.com>
2022-08-30 13:57:40 +0200

    f11 commit

--- src/components/news/events.js
+++ src/components/news/events.js
@@ -5,12 +5,23 @@ import { useInView } from 'react-intersection-observer';
 import { useEffect } from 'react';
 import {useAnimation} from 'framer-motion';
 import img1 from '../../images/received_630585981340841-1024x576.jpg'
+import axios from 'axios';
 export default function Events() {
+  const [events, setevents] = React.useState([]);
+  const show=()=>{
+    axios.get("/events/")
+    .then(response => {
+      const events = response.data[0];
+      setevents(events)
+      console.log(events)
+  
+  })};
     const{ref, inView}=useInView({
         threshold:0.2
       });
       const animation=useAnimation();
       useEffect(()=>{
+        show()
     if (inView){
       animation.start({
         x:0,
@@ -23,15 +34,19 @@ export default function Events() {
     }
       },[inView]);
   return (
-    <section ref={ref} class="events"> 
+    <section ref={ref} className="events"> 
     <h3>Evènements</h3>
         <motion.div 
-animate={animation} class="article">
+animate={animation} className="article">
        
-            <img class="image" src={img1}/>
-<p class="title"><span>20 Mars 2022</span><br/>9 h 00 min - 14 h 00 min</p>
+            <img className="image" src={"http://localhost:3000/uploadsevent/"+events.Image}/>
+<p className="title"><span>{events.Date}</span><br/>{events.Horaire}</p>
             
         </motion.div>
+        <motion.div initial={{x:'-100vw'}}
+animate={{x:'-10vw',y:'-40px'}}
+transition={{delay:1.5,duration:1.5,type:'spring',stiffness:120}}><motion.a   href="/events" className="homebutton" whileHover={{scale:1.1,boxShadow:"0px 0px 8px rgb(255,255,255"}} style={{marginLeft:"150px",marginTop:"60px"}} >Voir plus</motion.a>
+   </motion.div>  
     </section>
   )
 }
